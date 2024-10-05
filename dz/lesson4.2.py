1ec5c036932d306d7d767f36daf860bff858e533
Ilya Zatsepin <ilyadobrygreen@gmail.com>
2024-10-06 00:52:38 +0300

    finally

---
+++ dz/lesson4.2.py
@@ -0,0 +1,4 @@
+s = "wejwdwjnf(jdnjdnvjdnvj)sdkjfei"
+i = s.find("(")
+j = s.find(")")
+print(s[i+1:j])
---
+++ dz/test.py
@@ -0,0 +1,3 @@
+x= 'a b c c c c c v v v b b b n n n n v c c c b f d c c c c c c c'
+m = x.split(" ")
+print(m)
