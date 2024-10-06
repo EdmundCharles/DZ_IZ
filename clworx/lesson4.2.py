5f4c19906bfd5cdb6267c65926a455af69dea2ce
Ilya Zatsepin <ilyadobrygreen@gmail.com>
2024-10-06 13:26:33 +0300

    е

--- clworx/lesson4.2.py
+++ clworx/lesson4.2.py
@@ -1,11 +1,11 @@
 s = input()
 a = ''
 count = 0
-for i in range(len(s)) :
-    if s[i] == ':' :
+for i in s :
+    if i == ':' :
         count += 1
         a = (a + '%')
     else :
-        a = (a + s[i])
+        a = (a + i)
 print(a)
 print(f'Количество замен : {count}')
--- dz/lesson2.2.py
+++ dz/lesson2.2.py
@@ -1,15 +1,22 @@
-def is_number(z) :
-    try :
+def is_number(z):
+    try:
         float(z)
         return True
     except ValueError:
-        return False 
-x, y, *m = input("Введите два числа, которые необходимо поделить, через пробел \n").split( )
+        return False
+
+
+x, y, * \
+    m = input(
+        "Введите два числа, которые необходимо поделить, через пробел \n").split()
 if len(m) == 0:
-    if is_number(x) and is_number(y) :
-         x, y = map(float, (x,y))
-         print(f'{x}/{y} = {x/y}')    
-    else :
-         print('Вводите числа!')    
-else :
+    if is_number(x) and is_number(y):
+        if float(y) == 0:
+            print("На ноль делить нельзя")
+        else:
+            x, y = map(float, (x, y))
+            print(f'{x}/{y} = {x/y}')
+    else:
+        print('Вводите числа!')
+else:
     print('Введите ДВА числа!')
