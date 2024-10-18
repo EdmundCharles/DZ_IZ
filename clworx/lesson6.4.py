da0eb1ece054dde3ddf1b6ecd01af166a558fa52
Ilya Zatsepin <ilyadobrygreen@gmail.com>
2024-10-18 17:59:10 +0300

    18102024

--- clworx/lesson6.4.py
+++ clworx/lesson6.4.py
@@ -5,10 +5,14 @@ for i in range(n):
     for j in range(n):
         brr.append(int(input()))
     arr.append(brr)
-for i in range(1,n+1):
-    for j in range(n):
-        if i < j :
-            if arr[i][j] == arr[j][i]:
-                if 
 
-#нарисовать матрицу 5х5, так как может быть что индексы не свапаются. дальше думай...
+for i in range(n):
+    for j in range(n):
+        if j != i:
+            if arr[i][j] == arr[j][i]:
+                flag = 1
+            else :
+                flag = 0
+                break
+if flag == 1 : print('Symmetric!')
+else : print('Not symmetric!')
