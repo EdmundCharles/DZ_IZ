d232f1c9443293e604b4ad51e65eb821c9e76ac2
Ilya Zatsepin <ilyadobrygreen@gmail.com>
2024-11-15 17:29:09 +0300

    org

--- clworx/10.2.py
+++ clworx/10.2.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-df1 = pd.DataFrame({
+df = pd.DataFrame({
     'numbers1': [1,1,1,2,3,2,4,232,132],
     'numbers2': [1,234,543,23,43,45,23,1,4],
     'numbers3': [32,23,54,23,65,324,4556,342,34]})
-df1['numbe
+uns = df['numbers1'].compare(df['numbers2'])
+print(pd.concat([uns['self'],uns['other']]))
